a9019fc50520a9a787e88ad7ed8dc05e28a33cee
邓永庆 <dengyongqing@zhongan.io>
2017-12-15 16:20:42 +0800

    add try catch

--- strategies/start.py
+++ strategies/start.py
@@ -21,31 +21,35 @@ def start():
     read_sql_query = pd.read_sql_query('select * from my_stocks',con = engine)
     data = pd.DataFrame(read_sql_query)
     for index, row in data.iterrows():   # 获取每行的index、row
-            print('开始生成图片......' + row.code)
-            config = {
-              "base": {
-                "start_date": "2010-01-01",
-                "end_date": today,
-                "benchmark": "000300.XSHG",
-                "accounts": {
-                    "stock": row.code,
-                }
-              },
-              "extra": {
-                "log_level": "verbose",
-              },
-              "mod": {
-                "sys_analyser": {
-                  "enabled": True,
-                  "plot": False,
-                #   "output_file": '/Users/dengyongqing/my_work/one_strategy/examples/result.pkl',
-                  "plot_save_file": './one_data/static/' + row.code + '.png',
-                  # "plot_save_file": './static/' + row.code + '.png',
-                }
+        try:
+          print('开始生成图片......' + row.code)
+          config = {
+            "base": {
+              "start_date": "2010-01-01",
+              "end_date": today,
+              "benchmark": "000300.XSHG",
+              "accounts": {
+                  "stock": row.code,
+              }
+            },
+            "extra": {
+              # "log_level": "error",
+              "user_system_log_disabled": True,
+            },
+            "mod": {
+              "sys_analyser": {
+                "enabled": True,
+                "plot": False,
+              #   "output_file": '/Users/dengyongqing/my_work/one_strategy/examples/result.pkl',
+                "plot_save_file": './one_data/static/' + row.code + '.png',
+                # "plot_save_file": './static/' + row.code + '.png',
               }
             }
-            run_file(strategy_file_path, config)
-            print('生成图片成功......' + row.code)
+          }
+          run_file(strategy_file_path, config)
+          print('生成图片成功......' + row.code)
+        except Exception as e:
+          print(e)
 
 start()
 
